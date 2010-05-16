696d3a708397909bf17d1475ef8f57f8a48a3af0
rodrigomsc <rodrigomsc@8917a4ed-6c81-eb0b-204f-69b11407a017>
2010-05-16 03:53:46 +0000

    Criando interface para o PSO para permitir mais de uma implementação do algorítmo.

---
+++ trunk/PSO/src/br/upe/dsc/pso/problems/IProblem.java
@@ -0,0 +1,15 @@
+package br.upe.dsc.pso.problemas;
+
+public interface IProblem {
+	static final double VALOR_MIN_DIMENSAO = 0.01;
+	
+	double getFitness(Double... dimensao);
+	
+	boolean comparaFitness(Double fitnessPBest, Double fitnessPosicaoAtual);
+	
+	int getNumeroDimensoes();
+	
+	double getLimiteInferior(int dimensao);
+	
+	double getLimiteSuperior(int dimensao);
+}
